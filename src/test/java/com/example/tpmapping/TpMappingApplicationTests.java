e63a6ca4f4fb88d6e3342904f9b10490ef36f316
abdelmounim-moumay <124405682+abdelmounim-moumay@users.noreply.github.com>
2024-03-27 23:45:24 +0000

    mappin2

---
+++ src/test/java/com/example/tpmapping/TpMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpmapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
